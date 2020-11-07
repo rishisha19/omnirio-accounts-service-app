abc9ac12df3ae3484b9e1404e8bddc57518442dc
Rishi <rishi.sharma@datami.com>
2020-11-07 18:04:39 +0530

    client discovery

--- src/main/java/org/omnirio/app/config/ApplicationProperties.java
+++ src/main/java/org/omnirio/app/config/ApplicationProperties.java
@@ -1,13 +1,28 @@
 package org.omnirio.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
-/**
- * Properties specific to Accounts App.
- * <p>
- * Properties are configured in the {@code application.yml} file.
- * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
- */
+import java.net.URI;
+
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String customerServiceRegistryName;
+
+    private String customerServiceUrl;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Bean
+    @Autowired
+    public URI getCustomerServiceUrl (String customerServiceRegistryName) {
+        return this.discoveryClient.getInstances(customerServiceRegistryName).get(0).getUri();
+    }
+
+    @Autowired
+    public void setCustomerServiceRegistryName (String customerServiceRegistryName) {
+        this.customerServiceRegistryName = customerServiceRegistryName;
+    }
 }
